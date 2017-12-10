09f7a472749bd732e95687648a6dd46b4600a3a3
ask417 <ask417@nyu.edu>
2017-12-10 11:45:11 -0500

    Creating object for random number generation with rollings counts and frequencies

--- RandomCountGenerator.py
+++ RandomCountGenerator.py
@@ -1,7 +1,16 @@
 import random
+from Queue import Queue
 from collections import defaultdict
 
+
 def weighted_selection(values_with_weights):
+    '''
+    Randomly select variables proportional to their corresponding weights
+
+    :param dict values_with_weights: Labels with their relative weight
+    :return: the randomly selected value according to the given distribution
+    :rtype: int
+    '''
     total = sum(values_with_weights.values())
     r = random.uniform(0, total)
     start = 0
@@ -9,6 +18,58 @@ def weighted_selection(values_with_weights):
         if start + v >= r:
             return k
         start += v
+
+class RandomCountGenerator:
+    """
+    Object for generating weighted random selections and tracking a rolling
+    element frequency
+
+    ...
+
+    Attributes
+    ----------
+    values_with_weights : dict
+        Labels with their relative weights
+
+    """
+    def __init__(self, values_with_weights):
+        self.last_100_elements = Queue(maxsize=100)
+        self.values_with_weights = values_with_weights
+        self.rolling_counts = {k:0 for k in self.values_with_weights.keys()}
+
+    def weighted_selection(self):
+        '''
+        Randomly select variables proportional to their corresponding weights
+
+        :param dict values_with_weights: Labels with their relative weight
+        :return: the randomly selected value according to the given distribution
+        :rtype: int
+        '''
+        total = sum(self.values_with_weights.values())
+        r = random.uniform(0, total)
+        start = 0
+        for k, v in self.values_with_weights.iteritems():
+            if start + v >= r:
+                self.add_element(k)
+                return k
+            start += v
+
+    def add_element(self, element):
+        if self.last_100_elements.qsize() < 100:
+            self.last_100_elements.put(element)
+            self.update_counts(element)
+        else:
+            removed = self.last_100_elements.get()
+            self.last_100_elements.put(element)
+            self.update_counts(element, removed)
+
+    def update_counts(self, added, removed = None):
+        if removed is not None:
+            self.rolling_counts[removed] -= 1
+        self.rolling_counts[added] += 1
+
+    def get_frequencies(self):
+        return {k:v/100.0 for k,v in self.rolling_counts.iteritems()}
 
 
 if __name__ == "__main__":
@@ -19,3 +80,11 @@ if __name__ == "__main__":
         counts[weighted_selection(probs)] += 1
 
     print counts
+
+    rcg = RandomCountGenerator(probs)
+
+    for i in range(100000):
+        rcg.weighted_selection()
+        if i%10000 == 0 :
+            print rcg.get_frequencies()
+
